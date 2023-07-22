444bfea4cf09a4ec0805ef9c17b91365ef368cf0
MSajidur-Rahman <132246083+MSajidur-Rahman@users.noreply.github.com>
2023-07-22 21:33:12 +0600

    Update yt_playlist_dl.py

--- yt_playlist_dl.py
+++ yt_playlist_dl.py
@@ -1,11 +1,8 @@
 import os
 
-path = r"{YOUR PATH}"
-url = r"{YOUR PLAYLIST URL}"
-
-for song in os.listdir(path):
+for song in os.listdir({YOUR PATH}):
     song = f"{path}\{song}"
     os.rename(song,song[:-4]+"mp3")
     
 
-#yt-dlp -x --audio-format mp3 -o "{path}\%(playlist_index)s - %(title)s.%(ext)s" "{url}"
+#yt-dlp -x --audio-format mp3 -o "{YOUR PATH}\%(playlist_index)s - %(title)s.%(ext)s" "{YOUR PLAYLIST URL}"
